e0ff8edb546fb38decc971c5c5db3c48f0b3210c
maxhyfull <angelimaximiliano@gmail.com>
2020-02-07 00:30:09 -0300

    Modificaciones en el menu

--- core/modules/user_menu.php
+++ core/modules/user_menu.php
@@ -50,9 +50,9 @@
                             <li class="dashboard_link"><a href="http://www.forum.goalmanageronline.com/index.php" target="_blank"><span class="glyphicon glyphicon-comment"></span>Forum</a></li>
                             <li class="dashboard_link"><a href="?module=news"><span class="glyphicon glyphicon-bullhorn"></span>News</a></li>
                             <li class="dashboard_link"><a href="?module=rules"><span class="glyphicon glyphicon-file"></span>Rules/ Guide</a></li>
-                            <?php if($re['asignado'] == 1){ ?>
+                            
                             <li class="dropdown dashboard_link">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-retweet"></span>Trade</a>
+                                <a href="#" class="dropdown-toggle" data-toggle="<?php if($re['asignado'] == 1){ echo 'dropdown';} ?>" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-retweet"></span>Trade</a>
                                 <ul class="dropdown-menu">
                                     <li><a href="?module=trade_list"><span class="glyphicon glyphicon-chevron-right"></span> Show Trade List</a></li>
                                     <li><a href="?module=sell_player"><span class="glyphicon glyphicon-chevron-right"></span> Sell Player in Trade</a></li>
@@ -60,17 +60,17 @@
                                 </ul>
                             </li>
                             <li class="dropdown dashboard_link">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="libs/images/market.png" class="icon-menu2 center-block">Market</a>
+                                <a href="#" class="dropdown-toggle" data-toggle="<?php if($re['asignado'] == 1){ echo 'dropdown';} ?>" role="button" aria-haspopup="true" aria-expanded="false"><img src="libs/images/market.png" class="icon-menu2 center-block">Market</a>
                                 <ul class="dropdown-menu">
                                     <li><a href="?module=premium_players"><span class="glyphicon glyphicon-chevron-right"></span> Buy Premium Players</a></li>
                                     <li><a href="?module=other_upgrades"><span class="glyphicon glyphicon-chevron-right"></span> Stadium or Team Upgrades</a></li>
                                 </ul>
                             </li>
-                            <li class="dashboard_link"><a href="?module=division_awards"><img src="libs/images/league.png" class="icon-menu2 center-block">Awards</a></li>
-                            <li class="dashboard_link"><a href="?module=ranking"><img src="libs/images/ranking.png" class="icon-menu2 center-block">Ranking</a></li>
-                            <li class="dashboard_link"><a href="?module=results"><span class="glyphicon glyphicon-eye-open"></span> Results</a></li>
+                            <li class="dashboard_link"><a href="<?php if($re['asignado'] == 1){ echo '?module=division_awards';}else{ echo '#';} ?>"><img src="libs/images/league.png" class="icon-menu2 center-block">Awards</a></li>
+                            <li class="dashboard_link"><a href="<?php if($re['asignado'] == 1){ echo '?module=ranking';}else{ echo '#';} ?>"><img src="libs/images/ranking.png" class="icon-menu2 center-block">Ranking</a></li>
+                            <li class="dashboard_link"><a href="<?php if($re['asignado'] == 1){ echo '?module=results';}else{ echo '#';} ?>"><span class="glyphicon glyphicon-eye-open"></span> Results</a></li>
                             <li class="dropdown dashboard_link">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="libs/images/shirt.png" class="icon-menu2 center-block">My Team</a>
+                                <a href="#" class="dropdown-toggle" data-toggle="<?php if($re['asignado'] == 1){ echo 'dropdown';} ?>" role="button" aria-haspopup="true" aria-expanded="false"><img src="libs/images/shirt.png" class="icon-menu2 center-block">My Team</a>
                                 <ul class="dropdown-menu">
                                     <li><a href="?module=formation"><span class="glyphicon glyphicon-chevron-right"></span> My Formation</a></li>
                                     <li><a href="?module=team_name"><span class="glyphicon glyphicon-chevron-right"></span> Change Team Name/Shield/Number</a></li>
@@ -78,8 +78,7 @@
                                     <li><a href="?module=fixture"><span class="glyphicon glyphicon-chevron-right"></span> My Fixture</a></li>
                                 </ul>
                             </li>
-                            <?php } ?>
-                            <li class="dashboard_link"><a href="?module=buy_coins"><span class="glyphicon glyphicon-plus-sign"></span>Buy Coins</a></li>
+                            <li class="dashboard_link"><a href="<?php if($re['asignado'] == 1){ echo '?module=buy_coins';}else{ echo '#';} ?>"><span class="glyphicon glyphicon-plus-sign"></span>Buy Coins</a></li>
                             <li class="dashboard_link"><a href="?module=referrals"><span class="glyphicon glyphicon-user"></span>Referrals</a></li>
                             <li class="dropdown dashboard_link">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span>Account</a>
