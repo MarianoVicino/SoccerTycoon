6a762eb9a43f7d2e926643f3abc4b58a904fdcdd
chicotemaa <45741883+chicotemaa@users.noreply.github.com>
2020-02-08 13:21:50 -0300

    cambio connection
    cambio direccion de la base de datos, y7 methodo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     ob_start();
 	global $HOME;
 	//$HOME = 'https://goalmanageronline.com/';
-    $HOME = 'http://localhost/SoccerTycoon/';
+    $HOME = 'http://localhost/proyecto 1/';
     include("core/models/class.Connection.php");
     $db=new Connection();
 ?>
@@ -15,7 +15,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link rel="SHORTCUT ICON" href="<?= $HOME; ?>libs/images/icon.ico"/>
         <link rel="stylesheet" href="<?= $HOME; ?>libs/bootstrap.min.css">
-        <link rel="stylesheet" href="<?= $HOME; ?>libs/styles.css?v=2">      
+        <link rel="stylesheet" href="<?= $HOME; ?>libs/styles.css?nocache=">      
 <meta name="description" content="GoalManager is a strategy soccer game, in which you have the posibility to convert your virtual currency into real money."/> 
         <?php 
             if(isset($_GET['module']))
