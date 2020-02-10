f5f2fc682630dfed1876b020e904e11db89057f0
chicotemaa <45741883+chicotemaa@users.noreply.github.com>
2020-02-09 23:52:17 -0300

    eliminar conceptos erroneos
    prueba de imputs

--- core/modules/menu_register.php
+++ core/modules/menu_register.php
@@ -1,5 +1,6 @@
 <?php global $HOME; ?>
 
+<<<<<<< HEAD
 <div class="menu-box">
     <div class="container-fluid nav-bg">
         <header>
@@ -125,3 +126,38 @@
         </header>
     </div>
 </div>
+=======
+        <div class="menu-box">
+            <div class="container-fluid nav-bg">
+                <header>
+                    <nav class="navbar navbar-default">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed btn-lg" id="show-hide" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                </button>
+                                <a href="<?= $HOME; ?>index.php">
+                                    <img svg version="1.1" id="Capa_1" class="logo_panel"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 560 288" style="enable-background:new 0 0 560 288;" xml:space="preserve">
+                                        <style type="text/css">
+	                                        .st0{font-family:'BookAntiqua-BoldItalic';}
+                                        	.st1{font-size:30px;color:#FFF;}
+                                        </style>
+                                    <text transform="matrix(1 0 0 1 125.9769 140.1446)" class="st0 st1">SOCCER TYCOON</text>
+                                         
+                                </a>
+                            </div>
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="border:0px transparent;">
+                                <ul class="nav navbar-nav navbar-right">
+                                     <li>&nbsp;&nbsp;</li>
+                                    <li><a class="btn btn-danger" id="open_login" data-toggle="modal" data-target="#modalLogin"><span class="glyphicon glyphicon-user"></span> LOGIN</a></li> 
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>             
+                </header>
+            </div>
+        </div> 
+>>>>>>> parent of 473a4d5... prueba formulario login
