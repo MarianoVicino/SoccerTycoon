6e3fc0056907c02c9f98a0e9ea65b645222fdd26
maxhyfull <angelimaximiliano@gmail.com>
2020-02-16 01:22:33 -0300

    Un cambio que me olvide

--- admin_zone/modules/ajax/select.php
+++ admin_zone/modules/ajax/select.php
@@ -3,9 +3,9 @@ require_once("../../../core/models/class.Connection.php");
 $db 		= new Connection();
 $arre = array();
 if($_POST['donde'] == "divisio"){
-	$sql = mysqli_query($db, "SELECT idDivisiones,nombre_div FROM `divisiones` WHERE Regiones_idRegiones='".$_POST['vvalor']."' ORDER BY nombre_div ASC");
+	$sql = mysqli_query($db, "SELECT idDivisiones,nombre_div FROM `Divisiones` WHERE Regiones_idRegiones='".$_POST['vvalor']."' ORDER BY nombre_div ASC");
 }else if($_POST['donde'] == "ligas"){
-	$sql = mysqli_query($db, "SELECT idLigas,nombre_li,equipos_reales FROM `ligas` WHERE Divisiones_idDivisiones='".$_POST['vvalor']."' ORDER BY nombre_li ASC");
+	$sql = mysqli_query($db, "SELECT idLigas,nombre_li,equipos_reales FROM `Ligas` WHERE Divisiones_idDivisiones='".$_POST['vvalor']."' ORDER BY nombre_li ASC");
 }
 while ($re = mysqli_fetch_array($sql)) {
 	$arre[] = $re;
