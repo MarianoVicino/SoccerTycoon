253978439e5c3436a759a0434b58c3019025587a
maxhyfull <angelimaximiliano@gmail.com>
2020-03-01 21:54:20 -0300

    cambios para que ande el facebook

--- core/modules/registerfacebook.php
+++ core/modules/registerfacebook.php
@@ -1,14 +1,14 @@
 <?php
-require_once("../models/class.User.php");
 include("../models/class.Connection.php");
+include("../models/class.User.php");
 $db2=new Connection();
 
 //Vemos si el usuario ya esta logeado
-$log = mysqli_query($db2, "SELECT * FROM `equipos` WHERE email='".$_POST['Email']."'");
+$log = mysqli_query($db2, "SELECT * FROM `Equipos` WHERE email='".$_POST['Email']."'");
 if($log->num_rows == 0){
 	$ref = 0;
 	$user=new User();
-	$ssq = mysqli_query($db2, "SELECT MIN(idRegiones) min, MAX(idRegiones) max FROM `regiones`");
+	$ssq = mysqli_query($db2, "SELECT MIN(idRegiones) min, MAX(idRegiones) max FROM `Regiones`");
 	$rree = mysqli_fetch_array($ssq);
 	$id_region = rand($rree['min'], $rree['max']);
 	if($_POST['referral'] == ""){
@@ -16,7 +16,7 @@ if($log->num_rows == 0){
 	}else{
 		$ref = GetUserIdByLogin($_POST['referral']);
 	}
-	$user->AddUser($id_region, $_POST['Full_Name'], $_POST['ID'], $_POST['Email'], $ref,false);
+	$user->AddUser($id_region, $_POST['Full_Name'], $_POST['ID'], $_POST['Email'], $ref,3);
 }else{
 	session_start();
 }
